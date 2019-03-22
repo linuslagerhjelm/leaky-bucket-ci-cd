4d4c30f2abacce3c76a0dfe2dab45e24c204cba1
Linus <linus.lagerhjelm@gmail.com>
2019-03-22 08:57:40 +0100

    Add class documentation

--- src/LeakyBucket.java
+++ src/LeakyBucket.java
@@ -27,6 +27,14 @@ public final class LeakyBucket<T> {
         this.MAX_FAIL = failsPerSecond;
     }
 
+    /**
+     * Monitors the specified operation and should the method fail rate exceed the provided fail rate,
+     * it will throw an exception.
+     * @param operation The consumer method to monitor
+     * @param failsPerSecond the error rate at, which exceeded, will throw
+     * @param <T> the type parameter for the consumer
+     * @return a LeakyBucket instance
+     */
     public static <T> LeakyBucket<T> monitor(Consumer<T> operation, long failsPerSecond) {
         if (buckets == null) initBucketMonitor();
         LeakyBucket<T> lb =  new LeakyBucket<>(operation, failsPerSecond);
@@ -34,10 +42,25 @@ public final class LeakyBucket<T> {
         return lb;
     }
 
+    /**
+     * Same as {@link LeakyBucket#monitor(Consumer, long)} but defaults to a error rate of 100 failed
+     * operations per second.
+     * @param operation The consumer method to monitor
+     * @param <T> the type parameter for the consumer
+     * @return a LeakyBucket instance
+     */
     public static <T> LeakyBucket<T> monitor(Consumer<T> operation) {
         return LeakyBucket.monitor(operation, 100);
     }
 
+    /**
+     * Makes a monitored call to the stored operation and keeps track of fails. Note that this method may
+     * throw two different types of exceptions, for when the monitored method fails and when the allowed fail
+     * rate is exceeded.
+     * @param arg the argument to pass to the method
+     * @throws OperationFailedException thrown when the monitored operation fails, wraps the cause of the error
+     * @throws BucketOverflowException thrown when the error rate is exceeded
+     */
     public void invoke(T arg) throws OperationFailedException, BucketOverflowException {
         try {
             operator.accept(arg);
@@ -49,22 +72,30 @@ public final class LeakyBucket<T> {
         }
     }
 
+    /**
+     * Get the current fail count. This method should probably not be used except for testing purposes
+     * @return current fail count
+     */
     int getCurrentFailCount() {
         return fails.get();
     }
 
+    /**
+     * De-registers this LeakyBucket instance from the monitor.
+     * Could be used to prevent memory leaks.
+     */
     public void destroy() {
         buckets.remove(this);
-    }
-
-    private void decreaseFails() {
-        fails.updateAndGet(i -> i > 0 ? i - 1 : 0);
     }
 
     private static void initBucketMonitor() {
         buckets = new CopyOnWriteArraySet<>();
         scheduler.scheduleAtFixedRate(() -> buckets.forEach(LeakyBucket::decreaseFails),
                 COUNTDOWN_RATE, COUNTDOWN_RATE, TimeUnit.MILLISECONDS);
+    }
+
+    private void decreaseFails() {
+        fails.updateAndGet(i -> i > 0 ? i - 1 : 0);
     }
 
 }
